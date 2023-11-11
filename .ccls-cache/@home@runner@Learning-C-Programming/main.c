ef6255c9d97fbd10427c192c7b76023bf7c95f0e
norkart1 <138743746+norkart1@users.noreply.github.com>
2023-11-11 05:14:29 +0000

    Division

--- .ccls-cache/@home@runner@Learning-C-Programming/main.c
+++ .ccls-cache/@home@runner@Learning-C-Programming/main.c
@@ -3,22 +3,6 @@
 
 int main(void) {
 
-
-  //SUM 
-  /*
-  int number1, number2, sum;
-  printf("Enter 2 numbers ");
-  scanf("%d %d", &number1, &number2);
-  sum = number1 + number2;
-  printf("Result is : %d " , sum);
-  */
-  // DIVISION FLOAT
-  /*
-    float num1, num2, num3, average;
-  printf("Enter 3 numbers ");
-  scanf(" %f %f %f", &num1, &num2, &num3);
-  average = (num1,num2,num3)/3;
-  printf("Result is : %f", average);*/
 //SWAPPING
 /*
   int a = 20, b = 50, temp;
---
+++ learn c/division_float.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  float num1,num2,num3, average;
+  printf(" Enter 3 Numbers : ");
+  scanf(" %f %f %f", &num1, &num2, &num3);
+  average = (num1, num2, num3 )/3;
+  printf(" Result is : %f", average);
+
+  return EXIT_SUCCESS;
+}
