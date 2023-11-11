784cddae8db9731f0a042984e1f2490f15ab8005
norkart1 <138743746+norkart1@users.noreply.github.com>
2023-11-11 00:52:27 +0000

    sum.c

--- learn c/sum.c
+++ learn c/sum.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(void) {
-  int number1, number1, sum;
+  int number1, number2, sum;
   printf("Enter 2 Numbers ");
   scanf(" %d %d", &number1, &number2);
   sum = number1 + number2;
