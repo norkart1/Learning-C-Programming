63cafab081d3c0a97d53656b3276d117b32ab5e3
norkart1 <138743746+norkart1@users.noreply.github.com>
2023-11-11 00:51:23 +0000

    sum.c

---
+++ learn c/sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int number1, number1, sum;
+  printf("Enter 2 Numbers ");
+  scanf(" %d %d", &number1, &number2);
+  sum = number1 + number2;
+  printf(" Result is : %d", sum);
+
+  return EXIT_SUCCESS;
+}
